1ad98d053fe3d258470a058470dacb378cd61c34
LankevichYauheni <lankevich.yauheni@gmail.com>
2019-08-01 00:43:05 +0300

    Initial version

---
+++ solution/BytesSumCounter/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BytesSumCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1
+            // Get command line args
+
+            // 2
+            // Check an input directory and store file names from the directory
+
+            // 3
+            // Divide files on groups:
+            // 1. Small size < N
+            // 2. Big size > N
+            // where N (Available operating memory)/(available CPUs number)
+
+            // 4
+            // Start Tasks to calculate Small size files
+            // Start Tasks to calculate Big size files
+
+            // 5
+            // Await all tasks finished
+
+            // 6
+            // Write result to XML-file
+        }
+    }
+}
